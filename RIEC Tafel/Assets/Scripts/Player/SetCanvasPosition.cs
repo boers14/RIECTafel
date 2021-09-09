b69c85859f5d8f8170c41d95b565ae3c3c405a7c
MSI\stend <stendb29@gmail.com>
2021-09-09 10:12:49 +0200

    Added VR and UI

--- RIEC Tafel/Assets/Scripts/Player/SetCanvasPosition.cs
+++ RIEC Tafel/Assets/Scripts/Player/SetCanvasPosition.cs
@@ -19,7 +19,7 @@ public class SetCanvasPosition : MonoBehaviour
     {
         if (Physics.Raycast(transform.position, transform.forward, out RaycastHit wallRay, Mathf.Infinity, layer))
         {
-            canvas.position = wallRay.transform.position;
+            canvas.position = wallRay.transform.position + wallRay.transform.up * 0.15f;
             Vector3 canvasRotation = Vector3.zero;
             canvasRotation.y = wallRay.transform.eulerAngles.y + 90;
             canvas.eulerAngles = canvasRotation;
